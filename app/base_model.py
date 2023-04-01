4247070d1c1df728b0f7bb9761f6b58fdbbba3e7
Leonid77LD <mla9906@mail.ru>
2023-04-01 12:30:51 +0300

    connect db (postgres) and add test table

---
+++ app/base_model.py
@@ -0,0 +1,11 @@
+import uuid
+
+from sqlalchemy import MetaData, Integer, String, UUID, DATE, DateTime, Table, Column
+
+metadata = MetaData()
+
+test = Table(
+    'test', metadata,
+    Column('test_col', UUID, primary_key=True, nullable=False, default=uuid.uuid4(), comment="Первичный Ключ"),
+    Column('test_name', String, nullable=True, comment="Test name")
+)
