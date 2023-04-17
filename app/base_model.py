5b8d0f09829b24dd91e111e01953f3ea158521d5
Leonid77LD <mla9906@mail.ru>
2023-04-17 23:28:21 +0300

    add tmp

--- app/base_model.py
+++ app/base_model.py
@@ -7,5 +7,6 @@ metadata = MetaData()
 test = Table(
     'test', metadata,
     Column('test_col', UUID, primary_key=True, nullable=False, default=uuid.uuid4(), comment="Первичный Ключ"),
-    Column('test_name', String, nullable=True, comment="Test name")
+    Column('test_name', String, nullable=True, comment="Test name"),
+    Column('url', String, nullable=True, comment="url")
 )
--- app/main.py
+++ app/main.py
@@ -25,18 +25,22 @@
 import ast
 import uuid
 
-from fastapi import FastAPI
-# from httpx import post, get
+from fastapi import FastAPI, Depends, Response
+from fastapi.responses import JSONResponse
+from pydantic import json
 from requests import get
-from sqlalchemy import insert
+from sqlalchemy import insert, select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.organizer.dbutils import execute_command_insert
-
+from googletrans import Translator
 
 from app.base_model import test
 
 # Start main app
 # Start local app in console "uvicorn app.main:app --reload"
+from database import get_async_session
+
 app = FastAPI(
     title="DND App"
 )
@@ -48,23 +52,47 @@ def test_req():
 
 
 @app.get('/test_sql')
-async def test_sql(name: str):
+async def test_sql(name: str, session: AsyncSession = Depends(get_async_session)):
 
     stmt = insert(test).values(test_col=uuid.uuid4(),
                                test_name=name)
-    await execute_command_insert(stmt)
-    return {"status": "success"}
+    await session.execute(stmt)
+    await session.commit()
+    return True
 
 
-@app.get('/dnd5e_test_res')
+@app.get('/get_all_classes')
+async def get_all_classes(session: AsyncSession = Depends(get_async_session)):
+    query = select(test).group_by(test.c.test_col)
+    res = await session.execute(query)
+    a = res.all()
+    print(a)
+    print(type(a))
+    res = {'a': a}
+    return JSONResponse(content=res)
+
+
+@app.get('/all_classes_5e')
 def test_dnd_api():
-    res = get('https://www.dnd5eapi.co/api/')
-    print(res.status_code)
+    res = get('https://www.dnd5eapi.co/api/classes/')
+    print(res.text)
     if res.status_code == 200:
         content = ast.literal_eval(res.text)
         print(content)
+        for i in content['results']:
+            stmt = insert(test).values(test_col=uuid.uuid4(),
+                                       test_name=i['name'],
+                                       url=i['url']
+                                       )
+            execute_command_insert(stmt)
+
     return res.status_code, res.content
-#
+
+
+
+# translate = Translator()
+# resp = translate.translate('cat', 'ru')
+
 # app.include_router(
 #     fastapi_users.get_auth_router(auth_backend),
 #     prefix="/auth",
